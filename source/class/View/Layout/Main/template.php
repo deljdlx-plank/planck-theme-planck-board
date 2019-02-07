126e03781fb6cb279a18a3148963cc45e54a37b5
Jean luc <you@example.com>
2019-02-07 12:38:10 +0100

    synchro

--- source/class/View/Layout/Main/template.php
+++ source/class/View/Layout/Main/template.php
@@ -12,12 +12,6 @@
     <title>PlanckeyBlog</title>
 
 
-
-
-        <link href="vendor/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
-        <link href="vendor/fontawesome-free/css/all.css" rel="stylesheet">
-
-
         <link href="theme/planck-theme-planck-board/asset/css/grid.css" rel="stylesheet">
         <link href="theme/planck-theme-planck-board/asset/css/dashboard.css" rel="stylesheet">
         <link href="theme/planck-theme-planck-board/asset/css/material-form.css" rel="stylesheet">
@@ -52,76 +46,29 @@
         <div class="container-fluid" id="main-content">
             <div id="phi-main-container"></div>
         </div>
-
-
-
-
-
-
     </div>
 
 
 
-
-    <!-- /#page-content-wrapper -->
 
 </div>
 
 
 
 
-<!-- Bootstrap core CSS -->
-
-<script src="vendor/jquery/dist/jquery.js"></script>
-
-<script src="vendor/jquery-ui/jquery-ui.js"></script>
-<link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.css"/>
-<link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.theme.css"/>
-
-
 <!-- Popper js -->
+<!--
 <script src="vendor/tether/dist/js/tether.js"></script>
 <script src="vendor/popper.min.js"></script>
-<!-- Bootstrap-4 js -->
-<script src="vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+//-->
+
 <!-- Menu Toggle Script -->
 
 <!--
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 //-->
 
-<script type="text/javascript" src="vendor/khi/bootstrap.js"></script>
-<script type="text/javascript" src="vendor/khi/source/AjaxForm.js"></script>
-<script type="text/javascript" src="vendor/khi/source/Application.js"></script>
-<script type="text/javascript" src="vendor/khi/source/Routing/Request.js"></script>
-<script type="text/javascript" src="vendor/khi/source/Routing/Route.js"></script>
-<script type="text/javascript" src="vendor/khi/source/Routing/Router.js"></script>
 
-
-<script type="text/javascript" src="vendor/khi/source/ModuleLoader.js"></script>
-
-
-
-<script type="text/javascript" src="vendor/planck-front/source/Planck.js"></script>
-<script type="text/javascript" src="vendor/planck-front/source/Modal.js"></script>
-<script type="text/javascript" src="vendor/planck-front/source/Router.js"></script>
-<script type="text/javascript" src="vendor/planck-front/source/Controller.js"></script>
-<script type="text/javascript" src="vendor/planck-front/source/Application.js"></script>
-
-
-<script type="text/javascript" src="vendor/planck-front/source/FileUploader.js"></script>
-<script type="text/javascript" src="vendor/planck-front/source/DataLayer.js"></script>
-<script type="text/javascript" src="vendor/planck-front/source/DataLayerEntry.js"></script>
-
-
-
-
-<script type="text/javascript" src="vendor/planck-front/source/Model/Repository.js"></script>
-<script type="text/javascript" src="vendor/planck-front/source/Model/Entity.js"></script>
-
-
-
-<script src="?/tool/i18n/api/get-all-packages&callback=initializeI18N"></script>
 
 
 
@@ -129,19 +76,17 @@
 <script id="phi-html-body-end"></script>
 
 
+<!--
+<script type="text/javascript" src="vendor/planck-front/source/FileUploader.js"></script>
+//-->
 
 
 
 
 
+
+<script src="?/tool/i18n/api/get-all-packages&callback=initializeI18N"></script>
 <script type="text/javascript" src="theme/planck-theme-planck-board/javascript/bootstrap.js"></script>
-
-
-
-
-
-
-
 
 
 
